9261028e1ca05c5bb66237617b27981922cd2883
Iqbaleh <48812891+Iqbaleh@users.noreply.github.com>
2019-03-23 15:51:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+Studentlist = getElementsByClassName(student-List)
+
+const showPage = showPage(list, page)
+
+const ListToPage = list.appendChild(page) //We append the list to the page
+var pageNumber = page + 1 // A variable that can go to the next page? Not sure here
+
+    for(var i = 0; i < List.length; i++) 
+    ListToPage
+    if {
+      StudentlistList <= 10;
+      return pageNumber;
+}

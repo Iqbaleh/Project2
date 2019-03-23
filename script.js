a97cfdfefed8e0b0312f16ca4c04006c508e6c12
Iqbaleh <48812891+Iqbaleh@users.noreply.github.com>
2019-03-23 21:37:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-Studentlist = getElementsByClassName(student-List)
 
-const showPage = showPage(list, page)
 
-const ListToPage = list.appendChild(page) //We append the list to the page
-var pageNumber = page + 1 // A variable that can go to the next page? Not sure here
 
-    for(var i = 0; i < List.length; i++) 
-    ListToPage
-    if {
-      StudentlistList <= 10;
-      return pageNumber;
+const pageNumbers = document.querySelector(".page"); //Select the page class 
+const studentList = document.getElementsByClassName(student-list) //We get the studentList
+const showPage = showPage(studentList, pageNumbers) //Showpage has 2 variables, studentList, pageNumbers, right?
+const upperLimit = studentList /  5; //The Upperlimit is equal to the list of 54/5 (which is almost 10 pr page, trying to do it step by step)
+{
+  for(i = 0;  i < studentList.length; i++) {  //loop, that takes the length of the studentList
+    show.addEventListener("click", () => { //eventlistener is created that whenever a user clicks the following code executes
+ 
+          if studentList <= upperLimit { //if the Upperlimit is more than the studentlimit 
+          upperLimit.style.display = block; // https://teamtreehouse.com/library/styling-elements , uses block so i assume it displays? or?
+
+                  else if (studentList > upperLimit) { //if the student list is larger than the upper limit 
+                      upperLimit.style.display = ; //little lost here
+                   
+                      
+      
+    }
+  }
+
+
+}
+}
+
+appendPageLinks{
+document.getElementById("pagination").addEventListener("click") => () { //It get the pagination ID, and wheneve click is pressed, it showpage is called?
+  showPage;
+}
+
 }

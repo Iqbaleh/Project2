fd4e5845193eee580c832d533d839014cb9a4857
Iqbaleh <48812891+Iqbaleh@users.noreply.github.com>
2019-03-23 22:46:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 
 
-
 const pageNumbers = document.querySelector(".page"); //Select the page class 
 const studentList = document.getElementsByClassName(student-list) //We get the studentList
 const showPage = showPage(studentList, pageNumbers) //Showpage has 2 variables, studentList, pageNumbers, right?
-const upperLimit = studentList /  5; //The Upperlimit is equal to the list of 54/5 (which is almost 10 pr page, trying to do it step by step)
+const upperLimit =studentList[0-11] // I assume it takes the student in the arrays to 0-11
 {
   for(i = 0;  i < studentList.length; i++) {  //loop, that takes the length of the studentList
     show.addEventListener("click", () => { //eventlistener is created that whenever a user clicks the following code executes
  
           if studentList <= upperLimit { //if the Upperlimit is more than the studentlimit 
-          upperLimit.style.display = block; // https://teamtreehouse.com/library/styling-elements , uses block so i assume it displays? or?
-
+              upperLimit.style.display = block; // https://teamtreehouse.com/library/styling-elements , uses block so i assume it displays? or?
+                   upperLimit.appendChild(pageNumbers); 
+            
+            
                   else if (studentList > upperLimit) { //if the student list is larger than the upper limit 
                       upperLimit.style.display = ; //little lost here
                    
